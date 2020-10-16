7458092ee9d001cfaef8dd613134942a4a516543
Bima Putra Lamanda <48901540+bimap98@users.noreply.github.com>
2020-10-16 15:44:27 +0700

    Create tukar_dollar_ke_rupiah.py

---
+++ tukar_dollar_ke_rupiah.py
@@ -0,0 +1,6 @@
+a = int(input("Mau tukar berapa dollar?: "))
+b = int(input("Rate 1 dollar ke rupiah sekarang?: "))
+c = print("Jadi dengan dollar","{}".format(a),"dan dengan rate rupiah","{}".format(b), "mendapatkan sebesar: Rp",a*b)
+
+
+
